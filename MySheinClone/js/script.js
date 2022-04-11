b66914214c39403dfde3a41456ba83a948ec5f7d
Mário Mendes <mariomendesdeveloper@gmail.com>
2022-04-11 17:14:23 +0100

    upd files

--- MySheinClone/js/script.js
+++ MySheinClone/js/script.js
@@ -1,30 +1,9 @@
-/*
-var thumbnails = document.getElementById('thumbnails');
-for (let i = 0; i < thumbnails.children.length; i++) {
-    console.log(thumbnails)
-}
-*/
-
-const thumb01 = document.querySelector('.thumb1')
-const thumb02 = document.querySelector('.thumb2')
-const thumb03 = document.querySelector('.thumb3')
-const thumb04 = document.querySelector('.thumb4')
-
+const thumbnails = document.querySelector('.thumbnails');
 const bigImg = document.querySelector('.bigImg')
 
-thumb01.addEventListener("mouseover", () => {
-    bigImg.src = "images/big1.png";
-})
-
-thumb02.addEventListener("mouseover", () => {
-    bigImg.src = "images/big2.png";
-})
-
-
-thumb03.addEventListener("mouseover", () => {
-    bigImg.src = "images/big3.png";
-})
-
-thumb04.addEventListener("mouseover", () => {
-    bigImg.src = "images/big4.png";
-})
+for (let i = 0; i < thumbnails.children.length; i++) {
+    thumbnails.children[i].addEventListener("mouseover", () => {
+        const path = `images/big${i+1}.png`
+        bigImg.src = path;
+    })
+}
